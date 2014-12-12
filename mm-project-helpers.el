3b1e4db89c372a5eb283120fb26db59838c1ec95
Myke Morgan <myke_morgan@yahoo.com>
2014-12-12 11:47:28 -0800

    Add helpful comment to mm-load-api-files referring to 'desktop' package

--- mm-project-helpers.el
+++ mm-project-helpers.el
@@ -1,7 +1,15 @@
 ;; $Id: mm-project-helpers.el,v 1.1 2005/09/14 19:11:40 mimorgan Exp $
 
 (defun mm-load-api-files ()
-  "Load a sampling of API related files into buffers"
+  "
+Load a sampling of API related files into buffers
+
+It's generally simpler to use the 'desktop' package to save and
+recover buffer states between emacs executions.
+
+But this function will do the job, as well as being a place to
+remember various important files.
+"
   (interactive)
   (save-excursion
 ;    (find-file (concat (getenv "BUILDTOP") "/package/server/nvppaymentserv/nvppaymentserv.pkg_def"))
