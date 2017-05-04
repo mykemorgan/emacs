46242b20630afb5b038a4064bcff91919dc80492
Myke Morgan <myke_morgan@yahoo.com>
2017-05-04 15:11:43 -0700

    Rename remove-tabs to conform to 'mm-' prefix.

--- remove-tabs.el
+++ remove-tabs.el
@@ -1,13 +1,12 @@
 ;;
-;; Soo, can someone tell me how to get emacs to automagically convert
-;; tabs to spaces when I save a file?
+;; emacs to automagically convert tabs to spaces when I save a file
 ;;
 
-(defun my-untabify-buffer ()
+(defun mm-untabify-buffer ()
   (interactive)
   (save-excursion
     (mark-whole-buffer)
     (untabify (point) (mark))))
 
-(add-hook 'write-file-hooks 'my-untabify-buffer)
+(add-hook 'write-file-hooks 'mm-untabify-buffer)
 
